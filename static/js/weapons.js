cd2fa6b4cfdb2b408b086341826c67cf7247f347
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2024-08-18 15:29:27 +0100

    Added enemy functionality

--- static/js/weapons.js
+++ static/js/weapons.js
@@ -1,11 +1,11 @@
 // Define weapon types
 const WEAPONS = {
-  standard: { speed: 300, bulletSprite: "bullet", damage: 1, fireRate: 600, collisionArea: [4, 4] }, // 300 shots per minute (1 per second)
+  standard: { speed: 300, bulletSprite: "bullet", damage: 50, fireRate: 600, collisionArea: [4, 4] }, // 300 shots per minute (1 per second)
   split: {
     speed: 300,
     pickupSprite: "pickup_split",
     bulletSprite: "bullet_split",
-    damage: 2,
+    damage: 40,
     fireRate: 300,
     collisionArea: [10, 10],
   },
@@ -13,7 +13,7 @@ const WEAPONS = {
     speed: 600,
     pickupSprite: "pickup_laser",
     bulletSprite: "bullet_laser",
-    damage: 2,
+    damage: 35,
     fireRate: 400,
     collisionArea: [15, 3],
   },
@@ -21,7 +21,7 @@ const WEAPONS = {
     speed: 200,
     pickupSprite: "pickup_rocket",
     bulletSprite: "bullet_rocket",
-    damage: 4,
+    damage: 100,
     fireRate: 100,
     collisionArea: [23, 9],
     explosionArea: 50,
